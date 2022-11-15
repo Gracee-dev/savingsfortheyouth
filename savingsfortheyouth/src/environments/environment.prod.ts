71d195e342d1cf8ee48cf79cedff41d46818d8d4
Gracee-dev <118176410+Gracee-dev@users.noreply.github.com>
2022-11-15 06:00:16 +0300

    Add files via upload

---
+++ savingsfortheyouth/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://dnqbwjjonxtpachatldu.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRucWJ3ampvbnh0cGFjaGF0bGR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgwNjk2NDgsImV4cCI6MTk4MzY0NTY0OH0.GRBUbLaCxfhuQIpjueZ-fZUJFwH36g4N8JVQF96spKY',
+}
